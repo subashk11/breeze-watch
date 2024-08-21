ba2fee3982a0c7e2967428cf5b774fe4279a5438
subashk11 <subashk1153@gmail.com>
2024-08-21 22:48:11 +0530

    bug fix for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './shared/components/home/home.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: DashboardComponent
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
     path:'home',
